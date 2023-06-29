6d69af702461550377e135b6cdf7772c52947454
Shkirenkoroma <shkirenkoroma@mail.ru>
2023-06-29 18:46:05 +0300

    fix/internship: added properties like blur for first block

--- index.js
+++ index.js
@@ -3,18 +3,20 @@ const burgerMenu = document.querySelector('.header__burgermenu ');
 const socialLinksMenu = document.querySelector('.social-links');
 const asideMenu = document.querySelector('.aside-menu');
 const cancelAsideMenu = document.querySelector('.aside-menu__cancel');
-
+const blurFirstBlock = document.querySelector('.first-block');
 
 function activateMenu() {
   burgerMenu.classList.toggle('activate');
   socialLinksMenu.classList.toggle('hidden');
   asideMenu.classList.toggle('activate');
+  blurFirstBlock.classList.toggle('bluring')
 };
 
 function cancelMenu() {
   burgerMenu.classList.remove('activate');
   socialLinksMenu.classList.remove('hidden');
   asideMenu.classList.remove('activate');
+  blurFirstBlock.classList.remove('bluring')
 }
 
 blockTrigger.addEventListener('click', activateMenu);
