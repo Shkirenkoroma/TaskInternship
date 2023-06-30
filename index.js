76fbb23a6495069fcaed70b6894dd25b7d88cde0
Shkirenkoroma <shkirenkoroma@mail.ru>
2023-06-30 17:28:44 +0300

    feature/internship: added a few properties for style css

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const socialLinksMenu = document.querySelector('.social-links');
 const asideMenu = document.querySelector('.aside-menu');
 const cancelAsideMenu = document.querySelector('.aside-menu__cancel');
 const blurFirstBlock = document.querySelector('.first-block');
+const form = document.getElementById('form');
 
 function activateMenu() {
   burgerMenu.classList.toggle('activate');
@@ -19,5 +20,52 @@ function cancelMenu() {
   blurFirstBlock.classList.remove('bluring')
 }
 
+function validation(form) {
+  function removeError(input) {
+    const parent = input.parentNode;
+    if (parent.classList.contains('error')) {
+      parent.querySelector('.error-label').remove()
+      parent.classList.remove('error')
+    }
+  }
+
+  function createError(input, text) {
+    const parent = input.parentNode;
+    const errorLabel = document.createElement('label');
+    errorLabel.classList.add('error-label');
+    errorLabel.textContent = text;
+    parent.classList.add('error')
+    parent.append(errorLabel)
+  }
+  let result = true;
+  const allInputs = form.querySelectorAll('input');
+
+
+  for (const input of allInputs) {
+    removeError(input)
+    if (input.value == '') {
+      input.style.border = 'transparent'
+      console.log('Ошибка поля');
+      createError(input, 'Поле не заполнено!')
+      result = false
+    }
+  }
+  return result
+}
+
+function sendingForm(e) {
+  e.preventDefault();
+  if (validation(this) == true) {
+    alert('Форма проверена успешно и отправлена!')
+  }
+  const formData = new FormData(form)
+  formData.append('message', document.querySelector('.contact-data__textarea').value)
+  //Адрес, на который отправляется запрос, используется как моковый
+  axios.post('https://httpbin.org/post', formData)
+    .then(res => console.log(res))
+    .catch(err => console.log(err))
+}
+
 blockTrigger.addEventListener('click', activateMenu);
 cancelAsideMenu.addEventListener('click', cancelMenu);
+form.addEventListener('submit', sendingForm)
